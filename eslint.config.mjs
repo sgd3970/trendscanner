7009ac2d9a3e21a6c8f085f3579a1fe9d038dc2e
sgd3970 <sgd3970@naver.com>
2025-04-18 21:17:24 +0900

    Update ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // 사용하지 않는 변수 무시
+      "@typescript-eslint/no-explicit-any": "off", // any 허용
+      "no-unused-vars": "off", // JS 전용 no-unused-vars도 함께 비활성화
+    },
+  },
 ];
 
 export default eslintConfig;
